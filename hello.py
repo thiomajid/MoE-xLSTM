ca9a2463ab25800f78f67096ffdfdaa5863e9aac
thiomajid <tchiombianomajid@gmail.com>
2025-01-27 00:46:03 +0100

    Add CUDA availability checks and update dependency versions in configuration files

--- hello.py
+++ hello.py
@@ -5,6 +5,10 @@ from moe_xlstm.modules.model import MoExLSTMForCausalLM
 from moe_xlstm.utils.weights import count_inference_parameters, count_parameters
 
 if __name__ == "__main__":
+    print(torch.cuda.is_available())
+    print(torch.__version__)
+    print(torch.randn(2).cuda())
+
     config = MoExLSTMConfig.from_yaml("./model_config.yaml")
 
     moe = MoExLSTMForCausalLM(config)
